c1228128d5d61f90adbac206cee4208fb1cb5674
NiL <nilphreakz7@outlook.com>
2025-12-12 10:28:26 +0800

    Add files via upload

---
+++ delete-user/del-ssh.sh
@@ -0,0 +1,16 @@
+#wget https://github.com/${GitUser}/
+GitUser="NiL070"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
+clear
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "User $Pengguna was removed."
+else
+        echo -e "Failure: User $Pengguna Not Exist."
+fi
